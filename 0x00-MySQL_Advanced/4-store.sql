beb1baa483713f8c785734d6e18942cc1c5d8a34
mwanyambu <jmwanyambu@gmail.com>
2024-03-22 09:32:06 +0000

    trigger

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -2,4 +2,4 @@
 CREATE TRIGGER quantity_order
 AFTER INSERT ON orders
 FOR EACH ROW
-UPDATE item SET quantity = quantity - NEW.number WHERE name = NEW.item_name;
+UPDATE item SET quantity = quantity - NEW.number WHERE name=NEW.item_name;
