730dc147a523e49e1225738fd817430fe716ab58
mwanyambu <jmwanyambu@gmail.com>
2024-03-23 09:46:26 +0000

    procedures

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,19 +1,14 @@
 -- create a procedure
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER $$
 
-CREATE PROCEDURE ComputeAvarageScoreForUser (
-	IN p_user_id INT
+CREATE PROCEDURE ComputeAverageScoreForUser(
+	IN user_id INT
 )
 BEGIN
-	DECLARE avarage_score DECIMAL(10, 2);
-
-	SELECT AVG(score) INTO avarage_score
-	FROM scores
-	WHERE user_id = p_user_id;
-
-	INSERT INTO avarage_scores(user_id, avarage_score)
-	VALUES (p_user_id, avarage_score)
-	ON DUPLICATE KEY UPDATE avarage_score = avarage_score;
+	DECLARE avarage_score FLOAT;
+	SET avarage_score = (SELECT AVG(score) FROM corrections AS C WHERE C.user_id=user_id);
+	UPDATE users SET avarage_scores = avarage_score WHERE id=p_user_id;
 END;
 
 $$
